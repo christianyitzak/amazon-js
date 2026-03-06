b59c20f5952917f7d22b56a165d999760c2423db
christianyitzak <christian.yitzak@gmail.com>
2026-03-06 18:32:07 +0700

    fix price formatting and radio selectors

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,10 +1,11 @@
 import { cart, addToCart } from "../data/cart.js";
 import { products } from "../data/products.js";
+import { formatCurrency } from "./utils/money.js";
 
 let productsHTML = '';
 
 products.forEach((product) => {
-	productsHTML += `
+  productsHTML += `
         <div class="product-container">
         <div class="product-image-container">
           <img class="product-image" src="${product.image}">
@@ -22,7 +23,7 @@ products.forEach((product) => {
         </div>
 
         <div class="product-price">
-          $${(product.priceCents / 100).toFixed(2)}
+          $${formatCurrency(product.priceCents)}
         </div>
 
         <div class="product-quantity-container">
@@ -56,36 +57,36 @@ products.forEach((product) => {
 });
 
 document.querySelector('.js-products-grid')
-	.innerHTML = productsHTML;
+  .innerHTML = productsHTML;
 
 function updateCartQuantity() {
-	let cartQuantity = 0;
+  let cartQuantity = 0;
 
-	cart.forEach((cartItem) => {
-		cartQuantity += cartItem.quantity;
-	});
+  cart.forEach((cartItem) => {
+    cartQuantity += cartItem.quantity;
+  });
 
-	document.querySelector('.js-cart-quantity')
-		.innerHTML = cartQuantity;
+  document.querySelector('.js-cart-quantity')
+    .innerHTML = cartQuantity;
 }
 
 function showAddedMessage(productId) {
-	const addToCartNotificationEl = document.querySelector(`.js-added-to-cart-${productId}`);
-	addToCartNotificationEl.classList
-		.add('show-added-to-cart');
+  const addToCartNotificationEl = document.querySelector(`.js-added-to-cart-${productId}`);
+  addToCartNotificationEl.classList
+    .add('show-added-to-cart');
 
-	setTimeout(() => {
-		addToCartNotificationEl.classList
-			.remove('show-added-to-cart');
-	}, 2000);
+  setTimeout(() => {
+    addToCartNotificationEl.classList
+      .remove('show-added-to-cart');
+  }, 2000);
 }
 
 document.querySelectorAll('.js-add-to-cart').forEach((button) => {
-	button.addEventListener('click', () => {
-		const { productId } = button.dataset;
+  button.addEventListener('click', () => {
+    const { productId } = button.dataset;
 
-		addToCart(productId);
-		updateCartQuantity();
-		showAddedMessage(productId);
-	});
+    addToCart(productId);
+    updateCartQuantity();
+    showAddedMessage(productId);
+  });
 });
