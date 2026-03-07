0f6f6a336269d0a907113b53d09090d5e2eafb40
christianyitzak <christian.yitzak@gmail.com>
2026-03-08 00:17:35 +0700

    make delivery options interactive

--- data/cart.js
+++ data/cart.js
@@ -93,5 +93,17 @@ export function updateQuantity(productId, newQuantity) {
 
     saveToStorage();
     calculateCartQuantity();
+}
 
+export function updateDeliveryOption(productId, deliveryOptionId) {
+    let matchingItem;
+
+    cart.forEach((cartItem) => {
+        if (productId === cartItem.productId) {
+            matchingItem = cartItem;
+        };
+    });
+
+    matchingItem.deliveryOptionId = deliveryOptionId;
+    saveToStorage();
 }
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,4 @@
-import { calculateCartQuantity, cart, removeFromCart, updateQuantity } from "../data/cart.js";
+import { calculateCartQuantity, cart, removeFromCart, updateDeliveryOption, updateQuantity } from "../data/cart.js";
 import { products } from "../data/products.js";
 import { deliveryOptions } from "../data/deliveryOptions.js";
 import formatCurrency from "./utils/money.js";
@@ -103,7 +103,9 @@ function deliveryOptionsHTML(matchingProduct, cartItem) {
     const isChecked = deliveryOption.id === cartItem.deliveryOptionId;
 
     html += `
-      <div class="delivery-option">
+      <div class="delivery-option js-delivery-option"
+      data-product-id="${matchingProduct.id}"
+      data-delivery-option-id="${deliveryOption.id}">
         <input type="radio" ${isChecked ? 'checked' : ''} 
           class="delivery-option-input" 
           name="delivery-option-${matchingProduct.id}"
@@ -164,3 +166,11 @@ document.querySelectorAll('.js-save-link')
       };
     });
   });
+
+document.querySelectorAll('.js-delivery-option')
+  .forEach((element) => {
+    element.addEventListener('click', () => {
+      const { productId, deliveryOptionId } = element.dataset;
+      updateDeliveryOption(productId, deliveryOptionId);
+    })
+  })
